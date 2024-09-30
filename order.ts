b0603c946e458a8d4d8b6c56910077e654323b6f
ghagevaibhav <ghagevaibhav@gmail.com>
2024-09-30 18:28:43 +0530

    little modifications in order.ts

--- order.ts
+++ order.ts
@@ -1,66 +1,24 @@
-const request = require('request');
-const crypto = require('crypto');
-const baseURL = 'https://api.coindcx.com';
-import { resolve } from 'bun';
-import {key, secret} from './config';
-
-export const createOrder = (side: 'buy' | 'sell', market : string, price: number, quantity: number, clientOrderId: string ) => {
-
-    return new Promise<void>((resolve, reject) => {
-        const body = {
-            side,
-            "order_type": "limit_order", //Toggle between a 'market_order' or 'limit_order'.
-            market,
-            "price_per_unit": price,
-            "total_quantity": quantity, //Replace this with the quantity you want
-            "timestamp": Math.floor(Date.now()), //This is the current timestamp in milliseconds.
-            "client_order_id": clientOrderId
-          }
-          
-        
-        const payload = new Buffer(JSON.stringify(body)).toString();
-        const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex'); 
-    
-        const options = {    
-            url: baseURL + "exchange/v1/wallets/transfer",
-            headers: {
-                'X-AUTH-APIKEY': key,
-                'X-AUTH-SIGNATURE': signature
-            },
-            json: true,
-            body: body
-        }
-    
-        request.post(options, (err: any, res: any, body: any) => {
-            if (err) {
-                console.log(err);
-                return;
-            }
-            console.log(body);
-            resolve();
-        }); 
-    
-    })
-
-}
-
-export const cancelOrder = () => {
-
-}
-
-export const cancelAll = (market: string) => {
-
+const request = require('request')
+const crypto = require('crypto')
+const baseurl = "https://api.coindcx.com"
+import { key, secret } from "./config";
+export const createOrder = (side: "buy" | "sell", market: string, price: number, quantity: number, clientOrderId: string) => {
     return new Promise<void>((resolve) => {
         const body = {
-            market, 
-            timestamp:  Math.floor(Date.now()),
+            side,
+            "order_type": "limit_order", 
+            market,
+            "price_per_unit": price,
+            "total_quantity": quantity,
+            "timestamp": Math.floor(Date.now()),
+            "client_order_id": clientOrderId
         }
-        
+    
         const payload = new Buffer(JSON.stringify(body)).toString();
         const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex')
-        
+    
         const options = {
-            url: baseURL + "/exchange/v1/orders/cancel_all",
+            url: baseurl + "/exchange/v1/orders/create",
             headers: {
                 'X-AUTH-APIKEY': key,
                 'X-AUTH-SIGNATURE': signature
@@ -68,10 +26,50 @@ export const cancelAll = (market: string) => {
             json: true,
             body: body
         }
-        
+    
         request.post(options, function(error: any, response: any, body: any) {
-            console.log(body);
+            if (error) {
+                console.log("error while cancelling orders");
+            } else {
+                console.log(body);
+            }
+            resolve();
+        })
+    
+    })
+    
+}
+export const cancelOrder = () => {
+}
+export const cancelAll = (market: string) => {
+    return new Promise<void>((resolve) => {
+        const body = {
+            market,
+            timestamp: Math.floor(Date.now())
+        }
+    
+        const payload = new Buffer(JSON.stringify(body)).toString();
+        const signature = crypto.createHmac('sha256', secret).update(payload).digest('hex')
+    
+        const options = {
+            url: baseurl + "/exchange/v1/orders/cancel_all",
+            headers: {
+                'X-AUTH-APIKEY': key,
+                'X-AUTH-SIGNATURE': signature
+            },
+            json: true,
+            body: body
+        }
+    
+        request.post(options, function(error: any, response: any, body: any) {
+            if (error) {
+                console.log("error while cancelling orders");
+            } else {
+                console.log("acnceleled all orders");
+                console.log(body);
+            }
             resolve();
         })
     })
+   
 }
